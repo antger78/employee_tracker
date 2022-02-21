b9ff284d8f646953bb977d3e685f8804b399a519
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-02-20 23:21:56 -0500

    now we're ammending the table from the command line

--- index.js
+++ index.js
@@ -16,7 +16,98 @@ const questions = () => {
             {name:'Add a role', value:'add_role'},
             {name:'Add a department', value:'add_department'}
             ]
-          },
+        },
+        {
+            type: 'input',
+            name: 'employeeFirstName',
+            message: 'What is the first name of the new employee?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_employee'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'employeeLastName',
+            message: 'What is the last name of the new employee?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_employee'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'employeeRoleId',
+            message: 'What is the role ID of the new employee?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_employee'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'employeeMangerId',
+            message: 'What is the Manager ID of the new employee?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_employee'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'roleTitle',
+            message: 'What is the new role title?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_role'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the new role salary (upto 7 dollar figures and 2 cent figures)?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_role'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'roleDepartmentId',
+            message: 'What is the new role department ID?',
+            when: ({whatToDo}) =>{
+                if(whatToDo === 'add_role'){
+                return true;
+                } else {
+                return false;
+                }
+            }
+
+        },
       ])
     };
 
@@ -52,9 +143,20 @@ questions()
         console.table(row)});
         break;
         case 'add_employee':
-            // console.log('I am in view departments');   
+            const newEmployeeAnswersArray = [answer.employeeFirstName, answer.employeeLastName, answer.employeeRoleId, answer.answeremployeeMangerId ];
+            console.log(newEmployeeAnswersArray);
             db.query(
-            'SELECT * FROM role;',(err,row) =>{
+            'INSERT INTO `employee`(first_name,last_name,role_id,manager_id) VALUES (?,?,?,?);', newEmployeeAnswersArray ,(err,row) =>{
+                if (err){
+                    console.log('ERROR')
+                };
+        console.table(row)});
+        break;
+        case 'add_role':
+            const newRoleAnswersArray = [answer.roleTitle, answer.roleSalary, answer.roleDepartmentId ];
+            console.log(newRoleAnswersArray);
+            db.query(
+            'INSERT INTO `role`(title, salary, department_id) VALUES (?,?,?);', newRoleAnswersArray ,(err,row) =>{
                 if (err){
                     console.log('ERROR')
                 };
