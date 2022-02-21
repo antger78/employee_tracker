d6615669e2bdeab3525c2c0d1cbe7efe0f94b3b6
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-02-21 01:17:21 -0500

    pw

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: 'Madden4life78!',
+  password: 'INSERT YOUR PASSWORD HERE',
   database: 'business_database'
 });
 
